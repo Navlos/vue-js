42e434d80a11551f1becfd06ada1552753f2ccae
frontedTee <adetunjitayo2019@gmail.com>
2023-02-05 13:54:56 -1200

    vue tutorial

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            title: 'How to not give a fuck',
+            age:35,
+            name:'Adetayo',
+        }
+    }
+})
+
+app.mount('#app')
